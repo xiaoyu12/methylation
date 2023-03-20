5caeefbcdbd904433e98c60c2773242e70d761c9
Xiaoyu Zhang <xiaoyu@csusm.edu>
2023-03-20 10:36:53 -0700

    add code to calc size Factor with DESeq2 instead of GLM modeling

--- stat_analy/dmc_gene_stat.R
+++ stat_analy/dmc_gene_stat.R
@@ -17,6 +17,14 @@ gene_regions_uq <- gene_regions %>% distinct(ID, .keep_all = TRUE)
 # load gene expr data from Bayesian analysis
 load("./RData/deg.RData")
 
+# Calculate scaling factor using DESeq2
+library(DESeq2)
+samples <- data.frame(samples = colnames(deg_data[, 2:7]))
+samples$strain <- c(rep("EH1516", 3), rep("EH217",3))
+dds <- DESeqDataSetFromMatrix(countData = deg_data[, 2:7], colData=samples, design = ~1)
+dds <- estimateSizeFactors(dds)
+log(sizeFactors(dds))
+
 #fix col names
 colnames(deg_data)[8:9] <- c("expr_base", "std")
 colnames(deg_data)[14:15] <- c("logfc", "std.1")
@@ -98,23 +106,26 @@ d <- gene_data_dmc
 # reshape the data so that each row has one gene expression
 # the first col assumed to be gid
 # the reshaped data start from col2 and have nsamples
+sf_v <- log(sizeFactors(dds))            # scaling factor vector of size 6
+strain <- c(rep(0, 3), rep(1, 3))  # 1: EH1516, 2: EH217
 reshape_data <- function(d, nsample, strain) {
   # the first sample
   d_reshaped <- d[, c(1, 2)]
   colnames(d_reshaped) <- c("gid", "expr")
   d_reshaped$sample <- 1
   d_reshaped$strain <- strain[1]
+  d_reshaped$sf <- sf_v[1]
   for (i in 2:nsample) {
     d_i <- d[, c(1, i+1)]
     colnames(d_i) <- c("gid", "expr")
     d_i$sample <- i
+    d_i$sf <- sf_v[i]
     d_i$strain <- strain[i]
     d_reshaped <- rbind(d_reshaped, d_i)
   }
   d_reshaped$id <- rep(1:nrow(d), nsample)
   return (d_reshaped)
 }
-strain <- c(rep(0, 3), rep(1, 3))  # 1: EH1516, 2: EH217
 
 # Test the relation between pos and neg DMCs with the gene expression
 test_Expr_DMC <- function(d) {
@@ -129,12 +140,13 @@ test_Expr_DMC <- function(d) {
   e_bar = log(median(d_reshaped$expr))
   # build the dat list for MCMC
   dat <- list (
-    G = d_reshaped$id,
-    E = d_reshaped$expr,
-    S = d_reshaped$sample,
-    T = d_reshaped$strain,
-    W = standardize(log(rep(d$width, 6))),
-    e_bar = rep(e_bar, nrow(d_reshaped)),
+    G = d_reshaped$id,              # gene id
+    E = d_reshaped$expr,            # expression count
+    S = d_reshaped$sample,          # sample ID
+    T = d_reshaped$strain,          # treatment, i.e. strain = 0 for EH1516, 1 for EH217
+    sf = d_reshaped$sf,             # scaling factor
+    W = standardize(log(rep(d$width, 6))),          # gene width
+    e_bar = rep(e_bar, nrow(d_reshaped)),           # global mean expressionb
     XC = normalize(log(d_reshaped$ncpg)),           # normalized log number of CpG sites
     XDP = normalize(log(d_reshaped$dmc_pos + 1)),   # normalized log number of dmc_pos sites
     XDN = normalize(log(d_reshaped$dmc_neg + 1)),   # normalized log number of dmc_neg sites
@@ -148,9 +160,10 @@ test_Expr_DMC <- function(d) {
     alist (
       E ~ dgampois(lambda, phi),
       # f[S]: log sample factor, e[G] log express of gene in treatment 1,
-      log(lambda) <- e_bar + f[S] + e[G] + bDP * XDP * T + bDN * XDN *T,
+      #log(lambda) <- e_bar + f[S] + e[G] + bDP * XDP * T + bDN * XDN *T,
+      log(lambda) <- e_bar + sf + e[G] + bDP * XDP * T + bDN * XDN *T,
       
-      vector[6]: f ~ normal(0, 0.2),
+      #vector[6]: f ~ normal(0, 0.2),
       vector[ng]: e ~ normal(0, 3),
       bDP ~ normal(0, 1.5),
       bDN ~ normal(0, 1.5),
@@ -160,7 +173,7 @@ test_Expr_DMC <- function(d) {
   return(m_Expr_DMC)
 }
 
-m_Expr_DMC <- test_Expr_DMC(gene_data_dmc[, 1:29])
+m_Expr_DMC_new <- test_Expr_DMC(gene_data_dmc[, 1:29])
 precis(m_Expr_DMC)
 
 # Multi-level Model for gene expression and DMCs
